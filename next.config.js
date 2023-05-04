72b8cfa32e3a14e29a0c314c668ae19dd6a57637
keenregen <keenregen@gmail.com>
2023-05-04 10:59:22 +0300

    v5.0.1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  webpack5: true, 
   webpack: (config) => {
     config.resolve.fallback ={ fs: false}; // the solution
     
